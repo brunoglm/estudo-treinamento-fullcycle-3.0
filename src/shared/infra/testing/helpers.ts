e8a310fd7c85e3afad8239e949f36b575ece0f0a
Bruno Gabriel <bruno.glm_@hotmail.com>
2025-04-06 00:09:33 -0300

    add content

---
+++ src/shared/infra/testing/helpers.ts
@@ -0,0 +1,25 @@
+import { Sequelize, SequelizeOptions } from 'sequelize-typescript';
+
+export function setupSequelize(options: SequelizeOptions = {}) {
+  let _sequelize: Sequelize;
+
+  beforeAll(async () => {
+    _sequelize = new Sequelize({
+      // ...Config.db(),
+      dialect: 'sqlite',
+      storage: ':memory:',
+      logging: false,
+      ...options,
+    });
+  });
+
+  beforeEach(async () => await _sequelize.sync({ force: true }));
+
+  afterAll(async () => await _sequelize.close());
+
+  return {
+    get sequelize() {
+      return _sequelize;
+    },
+  };
+}
