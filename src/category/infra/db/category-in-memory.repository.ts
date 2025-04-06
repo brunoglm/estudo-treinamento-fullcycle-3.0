c852d8065f9bd9da59685700a29cec05ead5123c
Bruno Gabriel <bruno.glm_@hotmail.com>
2025-04-05 21:49:12 -0300

    add content

--- src/category/infra/db/category-in-memory.repository.ts
+++ src/category/infra/db/category-in-memory.repository.ts
@@ -1,9 +1,27 @@
+import { SortDirection } from "../../../shared/domain/repository/search-params";
 import { Uuid } from "../../../shared/domain/value-objects/uuid.vo";
-import { InMemoryRepository } from "../../../shared/infra/testing/db/in-memory/in-memory.repository";
+import { InMemorySearchableRepository } from "../../../shared/infra/testing/db/in-memory/in-memory.repository";
 import { Category } from "../../domain/category.entity";
 
-export class CategoryInMemoryRepository extends InMemoryRepository<Category, Uuid> {
+export class CategoryInMemoryRepository extends InMemorySearchableRepository<Category, Uuid> {
+  sortableFields: string[] = ['name', 'created_at'];
+
   getEntity(): new (...args: any[]) => Category {
     return Category
+  }
+
+  protected async applyFilter(itens: Category[], filter: string | null): Promise<Category[]> {
+    if (!filter) {
+      return itens;
+    }
+
+    return itens.filter((item) =>
+      item.name.toLowerCase() === filter.toLowerCase()
+    );
+  }
+
+  protected applySort(itens: Category[], sort: string | null, sort_dir: SortDirection | null): Category[] {
+    return sort ? super.applySort(itens, sort, sort_dir) :
+      super.applySort(itens, 'created_at', 'desc');
   }
 }
