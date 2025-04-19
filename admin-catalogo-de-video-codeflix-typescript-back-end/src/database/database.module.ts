0bac7eab7e495c19262c95b6af51cfe067cf5c66
Bruno Gabriel <bruno.glm_@hotmail.com>
2025-04-18 21:49:11 -0300

    add content

--- admin-catalogo-de-video-codeflix-typescript-back-end/src/database/database.module.ts
+++ admin-catalogo-de-video-codeflix-typescript-back-end/src/database/database.module.ts
@@ -1,15 +1,41 @@
 import { CategoryModel } from '@core/category/infra/db/sequelize/category.model';
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { CONFIG_SCHEMA_TYPE } from 'src/config/config.module';
+
+const models = [CategoryModel];
 
 @Module({
   imports: [
-    SequelizeModule.forRoot({
-      dialect: 'sqlite',
-      host: ':memory:',
-      logging: false,
-      models: [CategoryModel],
-      autoLoadModels: true,
+    SequelizeModule.forRootAsync({
+      useFactory: (configService: ConfigService<CONFIG_SCHEMA_TYPE>) => {
+        const dbVendor = configService.get('DB_VENDOR');
+        if (dbVendor === 'sqlite') {
+          return {
+            dialect: dbVendor,
+            host: configService.get('DB_HOST'),
+            autoLoadModels: configService.get('DB_AUTO_LOAD_MODELS'),
+            logging: configService.get('DB_LOGGING'),
+            models,
+          }
+        }
+        if (dbVendor === 'mysql') {
+          return {
+            dialect: dbVendor,
+            host: configService.get('DB_HOST'),
+            port: configService.get('DB_PORT'),
+            username: configService.get('DB_USERNAME'),
+            password: configService.get('DB_PASSWORD'),
+            logging: configService.get('DB_LOGGING'),
+            autoLoadModels: configService.get('DB_AUTO_LOAD_MODELS'),
+            database: configService.get('DB_DATABASE'),
+            models,
+          }
+        }
+        throw new Error(`DB_VENDOR ${dbVendor} not supported`);
+      },
+      inject: [ConfigService],
     }),
   ],
 })
