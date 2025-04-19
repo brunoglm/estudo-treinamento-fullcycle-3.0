68e012807fe65c559c4b34f9f6aff01d64430006
Bruno Gabriel <bruno.glm_@hotmail.com>
2025-04-19 13:56:55 -0300

    add content

---
+++ admin-catalogo-de-video-codeflix-typescript-back-end/src/nest-modules/categories-module/categories.presenter.ts
@@ -0,0 +1,15 @@
+import { CategoryOutput } from "@core/category/application/use-cases/common/category-output"
+
+export class CategoryPresenter {
+  id: string
+  name: string
+  description: string | null
+  created_at: Date
+
+  constructor(output: CategoryOutput) {
+    this.id = output.id
+    this.name = output.name
+    this.description = output.description
+    this.created_at = output.created_at
+  }
+}
