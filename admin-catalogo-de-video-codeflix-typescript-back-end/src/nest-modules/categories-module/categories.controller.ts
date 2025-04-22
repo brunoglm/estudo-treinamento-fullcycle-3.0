86f37d86fdefa67a79a94a4ce8a2f31865f0c68f
Bruno Gabriel <bruno.glm_@hotmail.com>
2025-04-21 21:48:16 -0300

    add content

--- admin-catalogo-de-video-codeflix-typescript-back-end/src/nest-modules/categories-module/categories.controller.ts
+++ admin-catalogo-de-video-codeflix-typescript-back-end/src/nest-modules/categories-module/categories.controller.ts
@@ -1,13 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Inject, ParseUUIDPipe, HttpCode } from '@nestjs/common';
-import { CreateCategoryDto } from './dto/create-category.dto';
-import { UpdateCategoryDto } from './dto/update-category.dto';
 import { CreateCategoryUseCase } from '@core/category/application/use-cases/create-category/create-category.use-case';
-import { UpdateCategoryUseCase } from '@core/category/application/use-cases/update-category/update-category.use-case';
 import { DeleteCategoryUseCase } from '@core/category/application/use-cases/delete-category/delete-category.use-case';
 import { GetCategoryUseCase } from '@core/category/application/use-cases/get-category/get-category.use-case';
 import { ListCategoriesUseCase } from '@core/category/application/use-cases/list-category/list-categories.use-case';
-import { CategoryPresenter } from './categories.presenter';
-import { CategoryOutput } from '@core/category/application/use-cases/common/category-output';
+import { UpdateCategoryUseCase } from '@core/category/application/use-cases/update-category/update-category.use-case';
+import { Body, Controller, Delete, Get, HttpCode, Inject, Param, ParseUUIDPipe, Patch, Post, Query } from '@nestjs/common';
+import { CategoryCollectionPresenter, CategoryPresenter } from './categories.presenter';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { SearchCategoriesDto } from './dto/search-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
 
 @Controller('categories')
 export class CategoriesController {
@@ -31,7 +31,7 @@ export class CategoriesController {
   @Post()
   async create(@Body() createCategoryDto: CreateCategoryDto) {
     const output = await this.createUseCase.execute(createCategoryDto)
-    return CategoriesController.serialize(output)
+    return CategoryPresenter.serialize(output)
   }
 
   @Patch(':id')
@@ -43,7 +43,7 @@ export class CategoriesController {
       id,
       ...updateCategoryDto,
     })
-    return CategoriesController.serialize(output)
+    return CategoryPresenter.serialize(output)
   }
 
   @HttpCode(204)
@@ -55,15 +55,12 @@ export class CategoriesController {
   @Get(':id')
   async findOne(@Param('id', new ParseUUIDPipe({ errorHttpStatusCode: 422 })) id: string) {
     const output = await this.getUseCase.execute({ id })
-    return CategoriesController.serialize(output)
+    return CategoryPresenter.serialize(output)
   }
 
   @Get()
-  findAll() {
-    return {} as any
-  }
-
-  static serialize(output: CategoryOutput): CategoryPresenter {
-    return new CategoryPresenter(output)
+  async search(@Query() searchCategoriesDto: SearchCategoriesDto) {
+    const output = await this.listUseCase.execute(searchCategoriesDto)
+    return CategoryCollectionPresenter.serialize(output)
   }
 }
--- admin-catalogo-de-video-codeflix-typescript-back-end/src/nest-modules/categories-module/categories.presenter.ts
+++ admin-catalogo-de-video-codeflix-typescript-back-end/src/nest-modules/categories-module/categories.presenter.ts
@@ -1,5 +1,7 @@
 import { CategoryOutput } from "@core/category/application/use-cases/common/category-output"
+import { ListCategoriesOutput } from "@core/category/application/use-cases/list-category/list-categories.use-case"
 import { Transform } from "class-transformer"
+import { CollectionPresenter } from "../shared-module/collection.presenter"
 
 export class CategoryPresenter {
   id: string
@@ -13,5 +15,23 @@ export class CategoryPresenter {
     this.name = output.name
     this.description = output.description
     this.created_at = output.created_at
+  }
+
+  static serialize(output: CategoryOutput): CategoryPresenter {
+    return new CategoryPresenter(output)
+  }
+}
+
+export class CategoryCollectionPresenter extends CollectionPresenter<CategoryPresenter> {
+  data: CategoryPresenter[]
+
+  constructor(output: ListCategoriesOutput) {
+    const { items, ...paginationProps } = output
+    super(paginationProps)
+    this.data = items.map((item) => CategoryPresenter.serialize(item))
+  }
+
+  static serialize(output: ListCategoriesOutput): CategoryCollectionPresenter {
+    return new CategoryCollectionPresenter(output)
   }
 }
