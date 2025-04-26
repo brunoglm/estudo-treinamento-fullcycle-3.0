32eb32917ce78c45222645a99a0f518ed51c3d1e
Bruno Gabriel <bruno.glm_@hotmail.com>
2025-04-26 20:07:49 -0300

    add content

--- admin-catalogo-de-video-codeflix-typescript-back-end/src/app.module.ts
+++ admin-catalogo-de-video-codeflix-typescript-back-end/src/app.module.ts
@@ -3,6 +3,7 @@ import { CategoriesModule } from './nest-modules/categories-module/categories.mo
 import { DatabaseModule } from './nest-modules/database-module/database.module';
 import { ConfigModule } from './nest-modules/config-module/config.module';
 import { SharedModule } from './nest-modules/shared-module/shared.module';
+import { WrapperDataModule } from './nest-modules/shared-module/interceptors/wrapper-data/wrapper-data.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { SharedModule } from './nest-modules/shared-module/shared.module';
     DatabaseModule,
     CategoriesModule,
     SharedModule,
+    WrapperDataModule,
   ],
   controllers: [],
   providers: [],
--- admin-catalogo-de-video-codeflix-typescript-back-end/src/main.ts
+++ admin-catalogo-de-video-codeflix-typescript-back-end/src/main.ts
@@ -1,6 +1,9 @@
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { EntityValidationErrorFilter } from './nest-modules/shared-module/filters/entity-validation-error.filter';
+import { NotFoundErrorFilter } from './nest-modules/shared-module/filters/not-found-error.filter';
+import { WrapperDataInterceptor } from './nest-modules/shared-module/interceptors/wrapper-data/wrapper-data.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,7 +15,13 @@ async function bootstrap() {
   )
 
   app.useGlobalInterceptors(
-    new ClassSerializerInterceptor(app.get(Reflector))
+    new ClassSerializerInterceptor(app.get(Reflector)),
+    new WrapperDataInterceptor()
+  )
+
+  app.useGlobalFilters(
+    new NotFoundErrorFilter(),
+    new EntityValidationErrorFilter()
   )
 
   await app.listen(3000);
